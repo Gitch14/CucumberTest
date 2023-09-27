917bf28cd21c592fe9f8034d6748a8505b6ab803
VladyslavBukach <vladislav.bukach@geeksforless.com>
2023-09-27 19:22:18 +0300

    update

--- nodeJs/step/survey.steps.js
+++ nodeJs/step/survey.steps.js
@@ -2,37 +2,31 @@ const { Given, When, Then } = require('cucumber');
 const assert = require('assert');
 
 Given('the user opens the survey page', function () {
-        console.log("Opening the survey page");
-    return 'success';
+        console.log("the user opens the survey page");
 });
 
 When('the user first question {string}', function (question) {
-    console.log("Answering the first question: " + question);
-    return 'success';
+    console.log(question);
 });
 
 When('the user answer f {string}', function (answer) {
-    console.log("Answering with: " + answer);
-    return 'success';
+    console.log(answer);
 });
 
 When('the user second question {string}', function (question) {
-    console.log("Answering the second question: " + question);
-    return 'success';
+    console.log(question);
 });
 
 When('the user answer s {int}', function (year) {
-    console.log("Answering with: " + year);
-    return 'success';
+    console.log(year);
 });
 
 When('the user clicks button {string}', function (button) {
-    console.log("Clicking the button: " + button);
-    return 'success';
+    console.log(button);
 });
 
 Then('the user sees the message {string}', function (message) {
-    console.log("Verifying the message: " + message);
+    console.log(message);
     assert.strictEqual(message, "Thank you! Survey was saved");
-    return "pending";
+
 });
